dad9e3f8036de8e020e51a83338d04c34d38d970
Yunseop Kim <undefined@mealant.com>
2019-01-09 22:45:10 +0900

    채팅 예제 만듦

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h3>Socket.io Chat Example</h3>
+    <!-- <form class="form-inline"> -->
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="name" placeholder="Name" v-model="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="room" class="col-sm-2 control-label">Room</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="room" placeholder="Room" v-model="room">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="msg" class="col-sm-2 control-label">Message</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="msg" placeholder="Message" v-model="msg">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button class="btn btn-default" @click="clickButton">Send</button>
+      </div>
+    </div>
+    <ul id="chat">
+      <li v-for="(item, index) in chatList" :key="index">{{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  sockets: {
+    connect: function() {
+      // eslint-disable-next-line
+      console.log("socket connected");
+    },
+    chatMessage: function (data) {
+      this.chatList.push(data)
+    }
+  },
+  data () {
+    return {
+      name: "",
+      room: "",
+      msg: "",
+      chatList: []
+    }
+  },
+  methods: {
+    clickButton: function () {
+      // $socket is socket.io-client instance
+      this.$socket.emit("chatMessage", {
+        name: this.name,
+        userid: this.name,
+        msg: this.msg,
+        room: this.room
+      });
+      this.msg = ""
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://192.168.35.127:3000/chat'
+}))
 
 Vue.config.productionTip = false
 
